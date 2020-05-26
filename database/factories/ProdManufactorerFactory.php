90265e39f26ebbfffeaba86421d8b10ebaaeb7db
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-26 20:44:14 -0300

    New - factory Manufacturer

---
+++ database/factories/ProdManufactorerFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use CStoke\Manufacturer;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Manufacturer::class, function (Faker $faker) {
+    return [
+        'description' => $faker->text(50),
+    ];
+});
