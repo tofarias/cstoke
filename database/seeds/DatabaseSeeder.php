aa16015ff4c5049cebfe461128ec99b9e2a2182a
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-26 20:45:00 -0300

    Add - mManufacturer factory ao executar seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         factory(CStoke\User::class)->create();
+
         factory(CStoke\Category::class)->create(['name' => 'diversão']);
         factory(CStoke\Category::class)->create(['name' => 'saúde']);
         factory(CStoke\Category::class)->create(['name' => 'informática']);
@@ -22,5 +23,17 @@ class DatabaseSeeder extends Seeder
         factory(CStoke\Category::class)->create(['name' => 'brinquedos e jogos']);
         factory(CStoke\Category::class)->create(['name' => 'automotivo']);
         factory(CStoke\Category::class)->create(['name' => 'alimentos e bebidas']);
+
+        factory(CStoke\Manufacturer::class)->create(['name' => 'bambozzi']);
+        factory(CStoke\Manufacturer::class)->create(['name' => 'boshi']);
+        factory(CStoke\Manufacturer::class)->create(['name' => 'sony']);
+        factory(CStoke\Manufacturer::class)->create(['name' => 'lg']);
+        factory(CStoke\Manufacturer::class)->create(['name' => 'bombril']);
+        factory(CStoke\Manufacturer::class)->create(['name' => 'positivo']);
+        factory(CStoke\Manufacturer::class)->create(['name' => 'avon']);
+        factory(CStoke\Manufacturer::class)->create(['name' => 'faber castel']);
+        factory(CStoke\Manufacturer::class)->create(['name' => 'cbs']);
+        factory(CStoke\Manufacturer::class)->create(['name' => 'abril']);
+        factory(CStoke\Manufacturer::class)->create(['name' => 'saraiva']);
     }
 }
