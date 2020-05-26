af38f668f9c44b2732a07eb000830a1129f61e34
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-26 17:42:57 -0300

    Add - category factory ao executar seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,14 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         factory(CStoke\User::class)->create();
+        factory(CStoke\Category::class)->create(['name' => 'diversão']);
+        factory(CStoke\Category::class)->create(['name' => 'saúde']);
+        factory(CStoke\Category::class)->create(['name' => 'informática']);
+        factory(CStoke\Category::class)->create(['name' => 'livros']);
+        factory(CStoke\Category::class)->create(['name' => 'roupas']);
+        factory(CStoke\Category::class)->create(['name' => 'cozinha']);
+        factory(CStoke\Category::class)->create(['name' => 'brinquedos e jogos']);
+        factory(CStoke\Category::class)->create(['name' => 'automotivo']);
+        factory(CStoke\Category::class)->create(['name' => 'alimentos e bebidas']);
     }
 }
