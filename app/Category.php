bc34a500e3dcbf271681fe10b4c92329ddbe9f42
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-26 17:41:22 -0300

    New - model categoty

---
+++ app/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CStoke;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $primaryKey = 'id';
+    protected $table = 'prod_category';
+    protected $attributes = [
+        'active' => true,
+    ];
+
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description',
+    ];
+}
