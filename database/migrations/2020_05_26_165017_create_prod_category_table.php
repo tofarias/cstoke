882e52105aa066a4aa0f5844998cc4fefddc581c
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-26 17:40:27 -0300

    New - migration: categoria de produtos

---
+++ database/migrations/2020_05_26_165017_create_prod_category_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prod_category', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->longText('description')->nullable();
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prod_category');
+    }
+}
