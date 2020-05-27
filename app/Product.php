b877d42ad7dd455b83fd1aacf1e718ba37c71184
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-26 22:31:14 -0300

    Add - model produto

---
+++ app/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CStoke;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $primaryKey = 'id';
+    protected $table = 'product';
+    protected $attributes = [
+        'active' => 1,
+        'lower_limit' => 100
+    ];
+
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'model','description'
+    ];
+
+
+    public function manufacturer(){
+        return $this->belongsTo(Manufacturer::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function createdBy(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function updatedBy(){
+        return $this->belongsTo(User::class);
+    }
+}
