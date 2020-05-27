56295e419ad7cf4afe83e52beba0ea07f2ccdd44
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-26 21:42:12 -0300

    New - migration: produto

---
+++ database/migrations/2020_05_26_165018_create_product_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->references('id')->on('prod_category');
+            $table->foreignId('manufacturer_id')->references('id')->on('prod_manufacturer');
+
+            $table->string('name');
+            $table->string('model');
+            $table->longText('description')->nullable();
+            $table->integer('minimum')->default(100);
+            $table->boolean('active')->default(1);
+            
+            $table->foreignId('created_by')->references('id')->on('users');
+            $table->foreignId('updated_by')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
