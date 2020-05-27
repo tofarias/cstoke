b7a482b9cc60ffb9ae9e2cb0e21aff900edbff4e
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-27 13:27:51 -0300

    Add - rotas para o relatório

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function () {    
     return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/report/{date}/productsin', 'ReportController@listProductsIn')->name('reportProductsIn');
+Route::get('/report/{date}/productsout', 'ReportController@listProductsOut')->name('reportProductsOut');
