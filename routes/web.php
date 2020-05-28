ad48d0bb296f314abf278f74bec5638a11fd1cb0
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-28 10:54:11 -0300

    FixUp - renomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -23,34 +23,34 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 // Relatórios
 
-Route::get('/report/{date}/productsin', 'ReportController@listProductsIn')->name('listProductsIn');
-Route::get('/report/{date}/productsout', 'ReportController@listProductsOut')->name('listProductsOut');
+Route::get('/relatorio/{date}/productsin', 'ReportController@listProductsIn')->name('listProductsIn');
+Route::get('/relatorio/{date}/productsout', 'ReportController@listProductsOut')->name('listProductsOut');
 
 // Cadatro de produtos
 
-Route::get('/products', 'ProductController@index')->name('product.list');
+Route::get('/produtos', 'ProductController@index')->name('product.list');
 
-Route::get('/products/new', 'ProductController@showRegisterForm')->name('product.showRegisterForm');
-Route::post('/products/new', 'ProductController@insert')->name('product.insert');
+Route::get('/produtos/novo', 'ProductController@showRegisterForm')->name('product.showRegisterForm');
+Route::post('/produtos/novo', 'ProductController@insert')->name('product.insert');
 
-Route::get('/products/{id}/edit', 'ProductController@showEditForm')->name('showEditForm');
-Route::post('/products/edit', 'ProductController@update')->name('product.update');
+Route::get('/produtos/{id}/editar', 'ProductController@showEditForm')->name('showEditForm');
+Route::post('/produtos/editar', 'ProductController@update')->name('product.update');
 
-Route::post('/products/{id}/delete', 'ProductController@delete')->name('product.delete');
+Route::post('/produtos/deletar', 'ProductController@delete')->name('product.delete');
 
-Route::post('/products/find', 'ProductController@ajaxFindProduct')->name('product.ajaxFindProduct');
+Route::post('/produtos/find', 'ProductController@ajaxFindProduct')->name('product.ajaxFindProduct');
 
 
 // Entrada de produtos
 
-Route::get('/productsin', 'ProductInController@index')->name('productin.list');
+Route::get('/produtosin', 'ProductInController@index')->name('productin.list');
 
-Route::get('/productsin/new', 'ProductInController@showRegisterForm')->name('productin.showRegisterForm');
-Route::post('/productsin/new', 'ProductInController@insert')->name('productin.insert');
+Route::get('/produtosin/novo', 'ProductInController@showRegisterForm')->name('productin.showRegisterForm');
+Route::post('/produtosin/novo', 'ProductInController@insert')->name('productin.insert');
 
-Route::get('/productsin/{id}/edit', 'ProductInController@showEditForm')->name('showEditForm');
-Route::post('/productsin/edit', 'ProductInController@update')->name('productin.update');
+Route::get('/produtosin/{id}/editar', 'ProductInController@showEditForm')->name('showEditForm');
+Route::post('/produtosin/editar', 'ProductInController@update')->name('productin.update');
 
-Route::post('/productsin/{id}/delete', 'ProductInController@delete')->name('productin.delete');
+Route::post('/produtosin/{id}/deletar', 'ProductInController@delete')->name('productin.delete');
 
 
