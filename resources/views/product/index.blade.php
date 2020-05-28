e50afc714e0d7272f1a64ecfa532c04ab834df13
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-28 07:07:19 -0300

    New - listagem de produtos

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('my_javascript_content')
+
+    $(function(){
+        $('.btn-delete[delete]').click(function(a){
+            if( confirm('Excluir o registro?') ){
+                
+                var id = $(this).attr('delete');
+                var urlTarget = '{!! route("product.delete", ["id" => '+id+']) !!}';
+                urlTarget = urlTarget.replace('+id+',id);
+                var token = $('[name="_token"]').val();
+
+                $.post( urlTarget, {'_token': token}, function( response ) {
+                    if(response.delete != null){
+                        if( response.delete ){
+                            alert(response.message);
+                            $('.btn-delete[delete='+response.id+']').closest('tr').remove();
+                        }
+                    }
+                });
+            }
+        });
+    });
+    
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Produtos Cadastrados</div>
+
+                <div class="card-body">
+                    
+                    {!! Form::token() !!}
+                    <table class="table table-striped">
+                        <thead class="thead-dark">
+                            <tr align="center">
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Categoria</th>
+                            <th scope="col">Modelo</th>
+                            <th scope="col">Fabricante</th>
+                            <th scope="col">Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach( $products as $key => $product )
+                            <tr title="{!! $product->id !!}">
+                                <th scope="row">{!! ($key+1) !!}</th>
+                                <td>{!! $product->name !!}</td>
+                                <td>{!! $product->category->name !!}</td>
+                                <td>{!! $product->model !!}</td>
+                                <td>{!! $product->manufacturer->name !!}</td>
+                                <td align="center">
+                                    <a href="{!! route('showEditForm',$product->id) !!}"><button type="button" class="btn btn-sm btn-info">Editar</button></a>
+                                    <button id="" delete="{!! $product->id !!}" type="button" class="btn-delete btn btn-sm btn-danger">Excluir</button>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
