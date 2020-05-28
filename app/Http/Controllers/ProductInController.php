aa618c43c312eeb53935611daa4f3389fac55ed9
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-28 13:50:55 -0300

    Del - removendo métodos não utilizados

--- app/Http/Controllers/ProductInController.php
+++ app/Http/Controllers/ProductInController.php
@@ -49,38 +49,4 @@ class ProductInController extends Controller
 
         return redirect()->route('listProductsIn',['2020-05-28']);
     }
-
-    public function showEditForm(Request $request, $id)
-    {
-        $product = Product::findOrFail($id);
-
-        $categories = Category::all();
-        $categories = $categories->pluck('name', 'id');
-
-        $manufacturer = Manufacturer::all();
-        $manufacturers = $manufacturer->pluck('name', 'id');
-
-        return view('productin.edit_form', compact('product', 'categories','manufacturers'));
-    }
-
-    public function update(Request $request)
-    {
-        $product = Product::findOrFail($request->id);
-        $product->fill($request->all());
-        $product->save();
-        
-        return redirect()->route('productin.list');
-    }
-
-    public function delete(Request $request)
-    {
-        $product = Product::findOrFail($request->id);
-        $product->delete();
-        
-        return response()->json([
-            'delete' => true,
-            'id' => $product->id,
-            'message' => 'Registro excluído com sucesso!'
-        ]);
-    }
 }
