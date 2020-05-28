6913722b94eaf3535bfecf9aea817a0692092902
Tiago Farias <tiago.farias.poa@gmail.com>
2020-05-28 10:36:32 -0300

    Add - rotas para cadastro da entrada de produto no estoque

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Relatórios
+
 Route::get('/report/{date}/productsin', 'ReportController@listProductsIn')->name('listProductsIn');
 Route::get('/report/{date}/productsout', 'ReportController@listProductsOut')->name('listProductsOut');
+
+// Cadatro de produtos
 
 Route::get('/products', 'ProductController@index')->name('product.list');
 
@@ -29,6 +34,23 @@ Route::get('/products/new', 'ProductController@showRegisterForm')->name('product
 Route::post('/products/new', 'ProductController@insert')->name('product.insert');
 
 Route::get('/products/{id}/edit', 'ProductController@showEditForm')->name('showEditForm');
-Route::post('/product/edit', 'ProductController@update')->name('product.update');
+Route::post('/products/edit', 'ProductController@update')->name('product.update');
 
-Route::post('/product/{id}/delete', 'ProductController@delete')->name('product.delete');
+Route::post('/products/{id}/delete', 'ProductController@delete')->name('product.delete');
+
+Route::post('/products/find', 'ProductController@ajaxFindProduct')->name('product.ajaxFindProduct');
+
+
+// Entrada de produtos
+
+Route::get('/productsin', 'ProductInController@index')->name('productin.list');
+
+Route::get('/productsin/new', 'ProductInController@showRegisterForm')->name('productin.showRegisterForm');
+Route::post('/productsin/new', 'ProductInController@insert')->name('productin.insert');
+
+Route::get('/productsin/{id}/edit', 'ProductInController@showEditForm')->name('showEditForm');
+Route::post('/productsin/edit', 'ProductInController@update')->name('productin.update');
+
+Route::post('/productsin/{id}/delete', 'ProductInController@delete')->name('productin.delete');
+
+
